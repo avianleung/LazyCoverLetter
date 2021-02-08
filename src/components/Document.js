d085d2373bfe526ee2b0a587844025e7e8d9b815
avianleung <avian.leung@gmail.com>
2021-02-08 02:23:54 -0500

    added font changeing

--- src/components/Document.js
+++ src/components/Document.js
@@ -9,6 +9,7 @@ const Document = () => {
   const p2 =
     "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 
+  const [font, setFont] = useState("Helvetica");
   const [name, setName] = useState("Your Name");
   const [number, setNumber] = useState("123-456-7890");
   const [email, setEmail] = useState("youremail@email.com");
@@ -87,7 +88,8 @@ const Document = () => {
     };
 
     var doc = new jsPDF();
-    doc.setFontSize(12);
+    doc.setFontSize(font === "Times" ? 12.5 : 12);
+    doc.setFont(font, "normal");
 
     //header
     doc.text(name, 185, 30, { align: "right" });
@@ -166,6 +168,19 @@ const Document = () => {
             <div className='col mb-4 mt-4'>
               <h1>Lazy Cover Letter</h1>
             </div>
+          </div>
+          <div className='row mb-4'>
+            <div className='col-1 mr-5'>Font:</div>
+            <select
+              className='col-2 form-select'
+              aria-label='Default select example'
+              onChange={(e) => setFont(e.target.value)}
+            >
+              <option selected value='Helvetica'>
+                Sans-Serif
+              </option>
+              <option value='Times'>Serif</option>
+            </select>
           </div>
           <div className='row mb-1'>
             <div className='col-1 mr-5'>Name:</div>
